4c99a059ffbb18aa71bcd5cbdbb298df01e69225
skweel <dissingardjino@icloud.com>
2024-09-30 13:48:16 +0200

    first commit

---
+++ firs_app1_gr1/AllPersonnView.swift
@@ -0,0 +1,18 @@
+//
+//  AllPersonnView.swift
+//  firs_app1_gr1
+//
+//  Created by Djino Dissingar on 30/09/2024.
+//
+
+import SwiftUI
+
+struct AllPersonnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllPersonnView()
+}
---
+++ firs_app1_gr1/loginView.swift
@@ -0,0 +1,18 @@
+//
+//  loginView.swift
+//  firs_app1_gr1
+//
+//  Created by Djino Dissingar on 30/09/2024.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginView()
+}
